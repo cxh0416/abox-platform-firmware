9f490269f1b2ba89105415dc933f298ded313de2
cxh <zxwl@56.com>
2026-08-10 10:28:47 +0800

    fix: support product linker and flash layout validation

--- cmake/ABoxPlatform.cmake
+++ cmake/ABoxPlatform.cmake
@@ -3,12 +3,12 @@ function(abox_platform_attach target)
         message(FATAL_ERROR "abox_platform_attach: target '${target}' does not exist")
     endif()
 
-    target_link_libraries(${target} PRIVATE abox::core)
+    target_link_libraries(${target} abox::core)
 
     if(ABOX_PLATFORM_SCHEDULER STREQUAL "FREERTOS")
-        target_link_libraries(${target} PRIVATE abox::port_freertos)
+        target_link_libraries(${target} abox::port_freertos)
     else()
-        target_link_libraries(${target} PRIVATE abox::port_baremetal)
+        target_link_libraries(${target} abox::port_baremetal)
     endif()
 
     if(ABOX_PRODUCT_CONFIG_DIR)
@@ -21,7 +21,7 @@ function(abox_platform_attach_boot target)
         message(FATAL_ERROR "abox_platform_attach_boot: target '${target}' does not exist")
     endif()
 
-    target_link_libraries(${target} PRIVATE abox::boot)
+    target_link_libraries(${target} abox::boot)
 
     if(ABOX_PRODUCT_CONFIG_DIR)
         target_include_directories(${target} PRIVATE "${ABOX_PRODUCT_CONFIG_DIR}")
